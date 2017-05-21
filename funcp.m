bbbc87a82aa2984d702dac9f7faaad62ff47a42a
haiyiliu314 <liuhai@umich.edu>
2017-05-20 21:08:48 -0400

    Add files via upload

--- funcp.m
+++ funcp.m
@@ -1,6 +1,6 @@
-function output = funcp(t, p)
-global Omegat gamma
-omega_g = 3.2 * 1.6e-19 ;
-% output=(omega_g - 1i * gamma) * p -Omegat(1);
-output=-1i*(( - 1i * gamma) * p -Omegat(t));
+function output = funcp(j, f, p)
+    global gamma y Ebind A dt tstart hbar sigmat
+    output = -1i*(-1i * gamma * p/Ebind- (1-2*f).*1e-3*hbar*(j<51))/hbar*Ebind;
+    % - (1-2*f).*1e-3*hbar*(j<51)
+%  output = -1i*((y'.^2.*p- (A*2*f).*p) -3*Ebind*p - 1i * gamma * p/Ebind - (1-2*f).*((A*p)+1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)))/hbar*Ebind;
 end
--- runge_kuttaf.m
+++ runge_kuttaf.m
@@ -1,9 +1,14 @@
-function g = runge_kuttaf(f1, g1, dt, i)
-
+function g = runge_kuttaf(f1, g1, i)
+    global dt
 %     t = dt * i;
     k1 = dt * funcf(i,g1);
-    k2 = dt * funcf(i, g1 + k1/2);
-    k3 = dt * funcf(i, g1 + k2/2);
+    k2 = dt * funcf(i+1/2, g1 + k1/2);
+    k3 = dt * funcf(i+1/2, g1 + k2/2);
     k4 = dt * funcf(i+1, g1 + k3);
     g = f1 + k1/6 +k2/3 + k3/3 + k4/6;
+end
+function output = funcf(j, p)
+ global Ebind dt sigmat tstart hbar
+ output=(imag((1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)).*p)*2)/hbar*Ebind; 
+%   output=(imag((1e-3*(j<51)).*p)*2)/hbar*Ebind;
 end
