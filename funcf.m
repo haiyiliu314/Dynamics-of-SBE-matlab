64acb538a5f8f041c2a41551643a852be2175541
haiyiliu314 <liuhai@umich.edu>
2017-05-24 10:34:32 -0400

    Add files via upload

--- funcf.m
+++ funcf.m
@@ -1,5 +1,5 @@
 function output = funcf(j, p)
- global Ebind dt sigmat tstart hbar
+ global Ebind dt sigmat tstart hbar A
  output=(imag((1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)).*p)*2)/hbar*Ebind; 
 %  output=(imag((1e-3*(j<51)/(50*dt)).*p)*2)*Ebind;
 % output = 0;
--- funcp.m
+++ funcp.m
@@ -1,6 +1,7 @@
 function output = funcp(j, f, p)
-    global gamma y Ebind A dt tstart sigmat hbar
+    global gamma y Ebind A dt tstart sigmat hbar shift
 %     output = -1i*(y'.^2.*p-1i * gamma * p/Ebind- (1-2*f).*1e-3*hbar*(j<51))/hbar*Ebind;
     % - (1-2*f).*1e-3*hbar*(j<51)
-  output = -1i*((y'.^2.*p- ((A*2*(f))).*p) -3*p - 1i * gamma * p/Ebind - (1-2*f).*((A*(p))+1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)))/hbar*Ebind;
+  output = -1i*(y'.^2.*p - (((A*2*(f))).*p) + shift*p - 1i * gamma * p/Ebind + (1-2*f).*((A*(p))+1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)))/hbar*Ebind;
+  % output = -1i*((y'.^2.*p- (((A*2*(f))).*p) - 3*p - 1i * gamma * p/Ebind - (1-2*f).*((A*(p))+1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)))/hbar*Ebind;*exp(1i*j*dt*Ebind/hbar)
 end
