adad4013ffa3a2f5e02d1e60384951207a293e32
haiyiliu314 <liuhai@umich.edu>
2017-05-24 14:59:16 -0400

    Add files via upload

--- funcp.m
+++ funcp.m
@@ -1,6 +1,8 @@
-function output = funcp(t, p)
-global Omegat gamma
-omega_g = 3.2 * 1.6e-19 ;
-% output=(omega_g - 1i * gamma) * p -Omegat(1);
-output=-1i*(( - 1i * gamma) * p -Omegat(t));
+function output = funcp(j, f, p)
+    global gamma y Ebind A dt tstart sigmat hbar shift
+%     output = -1i*(y'.^2.*p-1i * gamma * p/Ebind- (1-2*f).*1e-3*hbar*(j<51))/hbar*Ebind;
+    % - (1-2*f).*1e-3*hbar*(j<51)
+  output = -1i*(y'.^2.*p - (((A*2*(f))).*p) + shift*p - 1i * gamma * p/Ebind - (1-2*f).*((A*(p))+1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)))/hbar*Ebind;
+  % output = -1i*((y'.^2.*p- (((A*2*(f))).*p) - 3*p - 1i * gamma * p/Ebind - (1-2*f).*((A*(p))+1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)))/hbar*Ebind;*exp(1i*j*dt*Ebind/hbar)
+%    output = -1i*((y'.^2.*p- ((A*2*(f))).*p) -3*p - 1i * gamma * p/Ebind - (1-2*f).*((A*(p))+1e-3*exp(-(j*dt+tstart)^2/(sigmat)^2)))/hbar*Ebind;
 end
